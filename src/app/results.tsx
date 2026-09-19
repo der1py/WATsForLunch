f9af513b395c80ad158185a957906e85fcda8966
Eric Ning <e2ning@uwaterloo.ca>
2026-09-19 17:33:34 -0400

    add buildings to result page

--- src/app/results.tsx
+++ src/app/results.tsx
@@ -16,6 +16,14 @@ import {
 } from '@/services/recommendations';
 
 export default function ResultsScreen() {
+  const buildings = [
+    "EV3 Evergreen Cafe",
+    "ML Diner",
+    "REVelation",
+    "Mudie's",
+    "The Market at CMH",
+    "South Side Marketplace",
+  ];
   const params = useLocalSearchParams();
   const criteria = getSearchCriteriaFromParams(params);
   const recommendations = getTopRecommendations(criteria);
